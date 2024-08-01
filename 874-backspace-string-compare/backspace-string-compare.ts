f85ceef94e19704b33b1a481d78c0e275a2773b5
SillyCoder <beingfahad786@gmail.com>
2024-08-01 15:11:49 +0500

    Time: 54 ms (80.42%) | Memory: 52.5 MB (15.83%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.ts
+++ 874-backspace-string-compare/backspace-string-compare.ts
@@ -1,15 +1,24 @@
 function backspaceCompare(s: string, t: string): boolean {
-    const finalString = (input: string): string => {
-        let stack: string[] = [];
-        for (let char of input) {
-            if (char === '#') {
-                stack.pop();
-            } else {
-                stack.push(char);
-            }
+    let arr1 = [],arr2=[];
+    for(let i =0; i <s.length;i++){
+        if(isAlphaNumeric(s[i])){
+            arr1.push(s[i]);
+        }else{
+            arr1.pop()
         }
-        return stack.join('');
-    };
+    }
     
-    return finalString(s) === finalString(t);
+    for(let i =0; i <t.length;i++){
+        if(isAlphaNumeric(t[i])){
+            arr2.push(t[i]);
+        }else{
+            arr2.pop()
+        }
+    }
+    return JSON.stringify(arr1) ===JSON.stringify(arr2);
+};
+
+function isAlphaNumeric(str: string): boolean {
+    const regex = /^[a-z0-9]+$/i;
+    return regex.test(str);
 }
