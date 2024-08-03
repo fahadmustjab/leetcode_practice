3340d5b80e4aec88ac991e0faad564c3da1955d3
SillyCoder <beingfahad786@gmail.com>
2024-08-03 11:19:45 +0500

    Time: 56 ms (72.07%) | Memory: 51.6 MB (77.48%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.ts
+++ 874-backspace-string-compare/backspace-string-compare.ts
@@ -1,33 +1,32 @@
 function backspaceCompare(s: string, t: string): boolean {
-    let p1 = s.length - 1, p2 = t.length - 1, i = 0, n = Math.max(s.length, t.length);
-    while (p2 >= 0 || p1 >= 0) {
-        if (s[p1] === "#") {
+    let p1 =s.length-1, p2=t.length-1,i=0,n=Math.max(s.length,t.length);
+    while(p2 >=0 || p1 >=0){
+        if(s[p1] === "#"){
             let backCount = 2;
-            while (backCount > 0) {
+            while(backCount >0){
                 p1--;
                 backCount--;
-                if (s[p1] === "#") {
+                if(s[p1] === "#"){
                     backCount += 2;
                 }
-            }
-        } else if (t[p2] === "#") {
-            let backCount = 2;
-            while (backCount > 0) {
-                p2--;
-                backCount--;
-                if (t[p2] === "#") {
-                    backCount += 2;
-                }
-            }
-        } else {
-            if (s[p1] !== t[p2]) {
-                return false;
-            } else {
-                p1--;
-                p2--;
             }
         }
-
+        if(t[p2] === "#"){
+            let backCount = 2;
+            while(backCount >0){
+                p2--;
+                backCount--;
+                if(t[p2] === "#"){
+                    backCount += 2;
+                }
+            }
+        }
+       
+        if(s[p1] !== t[p2]){
+           return false;
+        }
+        p1--;
+        p2--;
     }
     return true;
 };
