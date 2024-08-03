121a60460699779a001062b802ce3aa4ae27d944
SillyCoder <beingfahad786@gmail.com>
2024-08-03 11:18:03 +0500

    Time: 65 ms (32.88%) | Memory: 51.5 MB (82.88%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.ts
+++ 874-backspace-string-compare/backspace-string-compare.ts
@@ -1,20 +1,34 @@
-function processBackspaces(input: string): string {
-    let k = 0;
-    let result = "";
-
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] === "#") {
-            k = Math.max(0, --k);
-            result = result.slice(0, k);
-        } else {
-            result += input[i];
-            k++;
-        }
-    }
-
-    return result;
-}
-
 function backspaceCompare(s: string, t: string): boolean {
-    return processBackspaces(s) === processBackspaces(t);
-}
+    let p1 = s.length - 1, p2 = t.length - 1, i = 0, n = Math.max(s.length, t.length);
+    while (p2 >= 0 || p1 >= 0) {
+        if (s[p1] === "#") {
+            let backCount = 2;
+            while (backCount > 0) {
+                p1--;
+                backCount--;
+                if (s[p1] === "#") {
+                    backCount += 2;
+                }
+            }
+        } else if (t[p2] === "#") {
+            let backCount = 2;
+            while (backCount > 0) {
+                p2--;
+                backCount--;
+                if (t[p2] === "#") {
+                    backCount += 2;
+                }
+            }
+        } else {
+            if (s[p1] !== t[p2]) {
+                return false;
+            } else {
+                p1--;
+                p2--;
+            }
+        }
+
+    }
+    return true;
+};
+
