fc2e5515af8052be2ed98415235246ab015a6685
SillyCoder <beingfahad786@gmail.com>
2024-08-04 17:37:18 +0500

    Time: 67 ms (37.37%) | Memory: 52.3 MB (31.94%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.ts
+++ 409-longest-palindrome/longest-palindrome.ts
@@ -1,14 +1,13 @@
 function longestPalindrome(s: string): number {
-    let hashMap: Record<string,number> = {},count=0,n=s.length;
-    for(let i =0;i<n;i++){
-        hashMap[s[i]] = (hashMap[s[i]] || 0) +1;
-        
+    let hashMap = new Map<string, number>(), count = 0, n = s.length;
+    for (let i = 0; i < n; i++) {
+        hashMap.set(s[i], (hashMap.get(s[i]) ?? 0) + 1);
     }
-    for(const [key,value] of Object.entries(hashMap)){
-        if(value %2 != 0)count++;
+    for (const [key, value] of hashMap) {
+        if (value % 2 !== 0) count++;
     }
-    if(count>1){
-        return n-count+1;
+    if (count > 1) {
+        return n - count + 1;
     }
     return n;
-};
+}
