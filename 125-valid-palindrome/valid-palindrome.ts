d750872babdd4ff0abb5e08108bd8b7c0d8342a9
SillyCoder <beingfahad786@gmail.com>
2024-08-04 18:30:48 +0500

    Time: 65 ms (72.21%) | Memory: 52.2 MB (91.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -1,11 +1,12 @@
 function isPalindrome(s: string): boolean {
     s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    let n = s.length;
-    let left=0,right=n-1;
-    while(left <= right){
+    let mid = Math.floor(s.length/2);
+    let left=mid-1;
+    let right = s.length %2 ===0 ? mid : mid+1;
+    while(left>=0 && right < s.length){
         if(s[left] !== s[right])return false;
-        right--;
-        left++;
+        left--;
+        right++;
     }
     return true;
 };
