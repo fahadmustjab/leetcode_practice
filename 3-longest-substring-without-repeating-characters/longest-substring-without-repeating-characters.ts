9077f202f60ac1e2db25d733a1c7930dfb78dadf
SillyCoder <beingfahad786@gmail.com>
2024-08-04 11:36:04 +0500

    Time: 78 ms (88.59%) | Memory: 54.2 MB (83.21%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,12 +1,12 @@
 function lengthOfLongestSubstring(s: string): number {
     if(s.length <=1)return s.length;
-    let max = 0,hashMap = {},p=0;
+    let max = 0,hashMap = new Map(),p=0;
     for(let i =0; i< s.length;i++){
-        const prevChar = hashMap[s[i]];
+        const prevChar = hashMap.get(s[i]);
         if(prevChar >=p){
             p = prevChar+1;
         }
-        hashMap[s[i]] = i;
+        hashMap.set(s[i],i);
         max = Math.max(max,i-p+1);
     }
     return max;
