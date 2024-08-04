54d8e39b72dbd2d54b25100c333ac078277428e2
SillyCoder <beingfahad786@gmail.com>
2024-08-04 13:46:28 +0500

    Time: 81 ms (33.47%) | Memory: 58.2 MB (25.52%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.ts
+++ 680-valid-palindrome-ii/valid-palindrome-ii.ts
@@ -1,21 +1,16 @@
 function validPalindrome(s: string): boolean {
-    const isPalindrome = (str: string, left: number, right: number): boolean => {
-        while (left < right) {
-            if (str[left] !== str[right]) {
-                return false;
-            }
+    const isPalindrome = (str: string,left:number,right:number):boolean =>{
+        while(left < right){
+            if(s[left] !== s[right])return false;
             left++;
             right--;
         }
         return true;
-    };
-
-    let left = 0, right = s.length - 1;
-
-    while (left < right) {
-        if (s[left] !== s[right]) {
-            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-        }
+    }
+    const n = s.length;
+    let left=0,right=n-1;
+    while(left<right){
+        if(s[left] !== s[right])return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
         left++;
         right--;
     }
