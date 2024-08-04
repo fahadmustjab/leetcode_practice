e11e77899337314fc47c2658c5e21ce6b23ed480
SillyCoder <beingfahad786@gmail.com>
2024-08-04 13:48:03 +0500

    Time: 74 ms (61.92%) | Memory: 57.4 MB (84.52%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.ts
+++ 680-valid-palindrome-ii/valid-palindrome-ii.ts
@@ -1,23 +1,25 @@
 function validPalindrome(s: string): boolean {
-    const isPalindrome = (str: string,left:number,right:number):boolean =>{
-        while(left < right){
-            if(s[left] !== s[right])return false;
+    // Helper function to check if a substring is a palindrome
+    const isPalindrome = (str: string, left: number, right: number): boolean => {
+        while (left < right) {
+            if (str[left] !== str[right]) {
+                return false;
+            }
             left++;
             right--;
         }
         return true;
-    }
-    const n = s.length;
-    let left=0,right=n-1;
-    while(left<right){
-        if(s[left] !== s[right])return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+    };
+
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        }
         left++;
         right--;
     }
 
     return true;
-}
-
-function removeCharAt(s: string, index: number): string {
-    return s.slice(0, index) + s.slice(index + 1);
 }
