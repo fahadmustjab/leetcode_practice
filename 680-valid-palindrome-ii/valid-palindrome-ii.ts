f2d99d3ea5dabc1498cc0ea605126e990a7d2453
SillyCoder <beingfahad786@gmail.com>
2024-08-04 13:38:53 +0500

    Time: 73 ms (67.78%) | Memory: 57.8 MB (56.49%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.ts
+++ 680-valid-palindrome-ii/valid-palindrome-ii.ts
@@ -1,34 +1,27 @@
 function validPalindrome(s: string): boolean {
-    const n = s.length;
-    if (n <= 2) return true;
-    let left = 0, right = n - 1;
-    while (left <= right) {
-        if (s[left] !== s[right] ) {
-            const string1 = removeCharAt(s, left);
-            const string2 = removeCharAt(s, right);
-            if(n<=3)return false;
-            else if(isPalindrome(string1))return true;
-            else if(isPalindrome(string2))return true;
-            else return false;
+    const isPalindrome = (str: string, left: number, right: number): boolean => {
+        while (left < right) {
+            if (str[left] !== str[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    };
+
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
         }
         left++;
         right--;
     }
-    return true;
-};
 
-function isPalindrome(s: string): boolean {
-    let n = s.length;
-    let left=0,right=n-1;
-    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    while(left <= right){
-        if(s[left] !== s[right])return false;
-        right--;
-        left++;
-    }
     return true;
-};
-
+}
 
 function removeCharAt(s: string, index: number): string {
     return s.slice(0, index) + s.slice(index + 1);
