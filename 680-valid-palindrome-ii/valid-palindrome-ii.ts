8d83f726ffb6e101bef61d96cd53f365c6d1379d
SillyCoder <beingfahad786@gmail.com>
2024-08-04 13:03:40 +0500

    Time: 97 ms (11.72%) | Memory: 61 MB (11.30%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.ts
@@ -0,0 +1,36 @@
+function validPalindrome(s: string): boolean {
+    const n = s.length;
+    if (n <= 2) return true;
+    let left = 0, right = n - 1;
+    while (left <= right) {
+        if (s[left] !== s[right] ) {
+            const string1 = removeCharAt(s, left);
+            const string2 = removeCharAt(s, right);
+            console.log(isPalindrome(string2));
+            if(n<=3)return false;
+            else if(isPalindrome(string1))return true;
+            else if(isPalindrome(string2))return true;
+            else return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+
+function isPalindrome(s: string): boolean {
+    let n = s.length;
+    let left=0,right=n-1;
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    while(left <= right){
+        if(s[left] !== s[right])return false;
+        right--;
+        left++;
+    }
+    return true;
+};
+
+
+function removeCharAt(s: string, index: number): string {
+    return s.slice(0, index) + s.slice(index + 1);
+}
