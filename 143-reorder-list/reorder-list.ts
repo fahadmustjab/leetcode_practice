7c4764cd4f6e50a4460deb9092bd97ca0863d8a7
SillyCoder <beingfahad786@gmail.com>
2024-08-11 14:48:24 +0500

    Time: 90 ms (43.99%) | Memory: 59.8 MB (46.55%) - LeetSync

--- 143-reorder-list/reorder-list.ts
+++ 143-reorder-list/reorder-list.ts
@@ -1,18 +1,3 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
-/**
- Do not return anything, modify head in-place instead.
- */
 function reorderList(head: ListNode | null): void {
     let slow=head,fast=head,prev=null,next,tmp1,tmp2;
     while(fast && fast.next){
