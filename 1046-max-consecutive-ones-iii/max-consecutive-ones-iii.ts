3d45ee0fbb4b924f61077bfd0e48d3ce50b90d59
SillyCoder <beingfahad786@gmail.com>
2024-08-14 01:59:19 +0500

    Time: 61 ms (92.56%) | Memory: 55.4 MB (56.47%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.ts
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.ts
@@ -1,12 +1,11 @@
 function longestOnes(nums: number[], k: number): number {
-    let max = 0, l = 0,z=0;
-    for(let r=0;r<nums.length;r++){
-        if(nums[r] === 0)z++;
-        while(z>k){
-            if(nums[l] === 0)z--;
+    let max = 0, l = 0,r=0, z = 0;
+    for ( r = 0; r < nums.length; r++) {
+        if (nums[r] === 0) k--;
+        if (k < 0) {
+            if(nums[l] === 0)k++;
             l++;
         }
-        max=Math.max(max,r-l+1);
     }
-    return max;
+    return r - l ;
 };
