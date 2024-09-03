41c35f0796f944b717715baa332f912bab244053
SillyCoder <beingfahad786@gmail.com>
2024-09-03 20:54:18 +0500

    Time: 170 ms (24.26%) | Memory: 87.8 MB (5.28%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.ts
+++ 234-palindrome-linked-list/palindrome-linked-list.ts
@@ -11,43 +11,16 @@
  */
 
 function isPalindrome(head: ListNode | null): boolean {
-    if (head === null || head.next === null) return true;
-    let middle = findMiddle(head);
-    let temp = middle
-    let reversed = reverse(temp);
-    return isEqual(head, reversed);
+    let current = head;
+    const traverse = (node: ListNode | null) => {
+        if(node === null){
+            return true;
+        }
+        const prevIsSame = traverse(node.next);
+        const currentIsSame = current.val === node.val;
+        current=current.next;
+        return prevIsSame && currentIsSame;
+    }
+    return traverse(head);
 };
 
-function isEqual(head1: ListNode, head2: ListNode) {
-    while (head1 && head2) {
-        if (head1.val !== head2.val) {
-            return false;
-        }
-        head1 = head1.next;
-        head2 = head2.next;
-    }
-    return true;
-}
-
-
-function findMiddle(head) {
-    let slow: ListNode = head;
-    let fast: ListNode = head;
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    return slow;
-}
-
-function reverse(head: ListNode): ListNode | null {
-    let prev: ListNode | null = null;
-    let next: ListNode | null = null;
-    while (head) {
-        next = head.next;
-        head.next = prev;
-        prev = head;
-        head = next;
-    }
-    return prev;
-}
