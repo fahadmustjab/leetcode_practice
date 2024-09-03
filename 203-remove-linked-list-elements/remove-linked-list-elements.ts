8baeea01aeca8d6a9f62851b1b656e3b7875d370
SillyCoder <beingfahad786@gmail.com>
2024-09-03 19:23:12 +0500

    Time: 73 ms (73.09%) | Memory: 57.1 MB (28.33%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.ts
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    if (!head) return null;
+    head.next = removeElements(head.next, val);
+    if (head.val === val) return head.next;
+    return head;
+};
+
