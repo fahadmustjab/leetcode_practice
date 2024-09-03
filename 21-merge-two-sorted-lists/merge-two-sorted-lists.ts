a282f0a4d403e27acd65347418f4d47659d58f7c
SillyCoder <beingfahad786@gmail.com>
2024-09-03 18:49:41 +0500

    Time: 66 ms (64.38%) | Memory: 51.9 MB (95.83%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -11,28 +11,20 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    let dummy: ListNode = new ListNode(0);
-    let merged = dummy;
+    let dummy: ListNode | null = new ListNode(0);
+    let merged: ListNode | null = dummy;
     if(list1 === null){
         return list2;
     }
     if(list2 === null){
-        return list1
+        return list1;
     }
-    while ((list1 && list2) ) {
-        if (list1.val < list2.val) {
-            merged.next = new ListNode(list1.val);
-            list1 = list1.next;
-        } else {
-            merged.next = new ListNode(list2.val);
-            list2 = list2.next;
-        }
-        merged = merged.next;
+    if(list1.val <= list2.val){
+        list1.next = mergeTwoLists(list1.next,list2);
+        return list1;
+    }else{
+        list2.next=mergeTwoLists(list1,list2.next);
+        return list2;
     }
-    if (list1) {
-        merged.next = list1;
-    }else if(list2){
-        merged.next = list2;
-    }
-    return dummy.next;
+    
 };
