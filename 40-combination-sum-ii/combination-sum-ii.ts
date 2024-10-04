65d6bf8dcdab1fd84f7746240b974d1f4a5f28f6
SillyCoder <beingfahad786@gmail.com>
2024-10-04 11:48:39 +0500

    Time: 74 ms (52.26%) | Memory: 52.8 MB (98.21%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.ts
+++ 40-combination-sum-ii/combination-sum-ii.ts
@@ -1,16 +1,13 @@
 function combinationSum2(candidates: number[], target: number): number[][] {
 
-    const result: Map<string, number[]> = new Map();
+    const result: number[][] = [];
     const n: number = candidates.length;
     candidates.sort((a, b) => a - b);
 
     const dfs = (i: number, target: number, subset: number[]) => {
         if (target < 0) return;
         if (target === 0) {
-            const str: string = subset.join('');
-            if (!result.has(str)) {
-                result.set(str, [...subset]);
-            }
+            result.push([...subset]);
             return;
         };
         for (let j = i; j < candidates.length; j++) {
