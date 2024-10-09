07c6424a20e07e8d3634b68d3eb9703f7ab43e07
SillyCoder <beingfahad786@gmail.com>
2024-10-09 22:41:58 +0500

    Time: 62 ms (22.74%) | Memory: 51 MB (24.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,11 +1,9 @@
 function climbStairs(n: number): number {
-    function memo(n,arr){
-        if(n <=1)return 1;
-        if(arr[n] != -1)return arr[n];
-        arr[n] = memo(n-1,arr)+memo(n-2,arr);
-        return arr[n];
+    const dp = Array(n+1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(let i =2;i <=n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
     }
-    let arr = Array(n+1).fill(-1);
-    return memo(n,arr);
-     
+    return dp[n];
 };
