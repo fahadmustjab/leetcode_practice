e0d909f4f5edad701c38f8d578195b8830406618
SillyCoder <beingfahad786@gmail.com>
2024-10-09 22:47:50 +0500

    Time: 61 ms (27.78%) | Memory: 50.1 MB (64.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,7 +1,8 @@
 function climbStairs(n: number): number {
+    if(n <=1)return 1;
     let prev1 = 1;
     let prev2 = 1;
-    for(let i =2;i <=n;i++){
+    for(let i =1;i <n;i++){
         const cur = prev1+prev2;
         prev2=prev1;
         prev1=cur;
