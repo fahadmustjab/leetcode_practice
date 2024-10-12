75d164a282b73e3cd78c19f44703d238c1cdad31
SillyCoder <beingfahad786@gmail.com>
2024-10-12 22:27:33 +0500

    Time: 78 ms (39.28%) | Memory: 54.7 MB (44.84%) - LeetSync

--- 46-permutations/permutations.ts
+++ 46-permutations/permutations.ts
@@ -1,15 +1,14 @@
 function permute(nums: number[]): number[][] {
-    let n: number = nums.length,result: number[][] = [];
-    function backtrack(index: number){
-        if(index >= nums.length){
+    let n: number = nums.length, result: number[][] = [];
+    function backtrack(index: number) {
+        if (index >= nums.length) {
             result.push([...nums]);
-            // console.log(nums);
             return;
         }
-        for(let i = index;i < n;i++){
-            swap(nums,i,index);
-            backtrack(index+1);
-            swap(nums,i,index);
+        for (let i = index; i < n; i++) {
+            swap(nums, i, index);
+            backtrack(index + 1);
+            swap(nums, i, index);
         }
     }
     backtrack(0);
@@ -17,5 +16,7 @@ function permute(nums: number[]): number[][] {
 };
 
 function swap(nums: number[], i: number, j: number) {
-    [nums[i], nums[j]] = [nums[j], nums[i]];
+    if (i != j) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
 }
