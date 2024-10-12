b576f014892dc1838413c684c03792a6dbb7662c
SillyCoder <beingfahad786@gmail.com>
2024-10-12 22:38:33 +0500

    Time: 62 ms (95.94%) | Memory: 53.4 MB (95.02%) - LeetSync

--- 46-permutations/permutations.ts
+++ 46-permutations/permutations.ts
@@ -1,22 +1,21 @@
 function permute(nums: number[]): number[][] {
     let n: number = nums.length, result: number[][] = [];
-    function backtrack(index: number) {
-        if (index >= nums.length) {
-            result.push([...nums]);
+    function backtrack(index: number,perms: number[],map: Map<number,boolean>) {
+        if (perms.length === nums.length) {
+            result.push([...perms]);
             return;
         }
-        for (let i = index; i < n; i++) {
-            swap(nums, i, index);
-            backtrack(index + 1);
-            swap(nums, i, index);
+        for (let i = 0; i < n; i++) {
+            if(!map.get(nums[i])){
+                map.set(nums[i],true);
+                perms.push(nums[i]);
+                backtrack(i+1,perms,map);
+                perms.pop();
+                map.set(nums[i],false);
+            }
         }
     }
-    backtrack(0);
+    const map: Map<number,boolean> = new Map();
+    backtrack(0,[],map);
     return result;
 };
-
-function swap(nums: number[], i: number, j: number) {
-    if (i != j) {
-        [nums[i], nums[j]] = [nums[j], nums[i]];
-    }
-}
