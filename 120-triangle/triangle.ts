5700102795906f7906a732c5abf5bd97865d8206
SillyCoder <beingfahad786@gmail.com>
2024-10-13 18:32:36 +0500

    Time: 57 ms (78.67%) | Memory: 52.8 MB (21.33%) - LeetSync

--- 120-triangle/triangle.ts
+++ 120-triangle/triangle.ts
@@ -1,14 +1,13 @@
 function minimumTotal(triangle: number[][]): number {
     const m = triangle.length;
-    const dp: number[][] = Array.from({ length: m }, (_, i) => Array(i + 1).fill(-1));
-    function backtrack(i: number, j: number) {
-        if (i === m - 1) {
-            return triangle[m - 1][j];
-        }
-        if (dp[i][j] != -1) return dp[i][j];
-        const down = backtrack(i + 1, j);
-        const diagonal = backtrack(i + 1, j + 1);
-        return dp[i][j] = triangle[i][j] + Math.min(down, diagonal);
+    const dp = Array(m).fill(0).map(() => Array(m).fill(-1));
+    for(let i =0;i < triangle[m-1].length;i++){
+        dp[m-1][i] = triangle[m-1][i];
     }
-    return backtrack(0, 0);
+    for(let i =m-2; i >=0;i--){
+        for(let j =0;j <triangle[i].length;j++){
+            dp[i][j] = triangle[i][j] + Math.min(dp[i+1][j],dp[i+1][j+1]);
+        }
+    }
+    return dp[0][0];
 };
