a96c04d813a46e4df2803ab098dc5dda6d892a87
SillyCoder <beingfahad786@gmail.com>
2024-10-13 13:40:48 +0500

    Time: 58 ms (61.03%) | Memory: 51.9 MB (32.85%) - LeetSync

--- 62-unique-paths/unique-paths.ts
+++ 62-unique-paths/unique-paths.ts
@@ -1,24 +1,10 @@
 function uniquePaths(m: number, n: number): number {
-    const dp = new Array(m);
-    for (let i = 0; i < m; i++) {
-        dp[i] = new Array(n).fill(-1);
-    }
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            let up = 0, left = 0;
-            if (i === 0 && j === 0) {
-                dp[0][0] = 1;
-            } else {
-                if (i > 0) {
-                    up = dp[i - 1][j]
-                }
-                if (j > 0) {
-                    left = dp[i][j - 1];
-                }
-                dp[i][j] = up + left;
-            }
+    const dp = new Array(m).fill(0).map(() => new Array(n).fill(1));
 
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
     return dp[m - 1][n - 1];
-};
+}
