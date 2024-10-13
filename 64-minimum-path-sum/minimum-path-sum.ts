58ca965c5b39cf7d290d17316e4bd80490007818
SillyCoder <beingfahad786@gmail.com>
2024-10-13 16:42:47 +0500

    Time: 80 ms (18.59%) | Memory: 53.9 MB (26.92%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.ts
+++ 64-minimum-path-sum/minimum-path-sum.ts
@@ -4,16 +4,16 @@ function minPathSum(grid: number[][]): number {
     const dp = Array(m).fill(0).map(() => Array(n).fill(-1));
     dp[0][0] = grid[0][0];
 
-    for (let j = 1; j < n; j++) {
-        dp[0][j] = dp[0][j - 1] + grid[0][j];
-    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === 0 && j === 0) {
+                dp[0][0] = grid[i][j];
+            } else {
+                const left = grid[i][j] + ( i > 0 ? dp[i - 1][j]: Number.MAX_SAFE_INTEGER);
+                const right = grid[i][j] + (j > 0 ?  dp[i][j - 1]: Number.MAX_SAFE_INTEGER);
+                dp[i][j] = Math.min(left, right);
+            }
 
-    for (let i = 1; i < m; i++) {
-        dp[i][0] = dp[i - 1][0] + grid[i][0];
-    }
-    for (let i = 1; i < m; i++) {
-        for (let j = 1; j < n; j++) {
-            dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
         }
     }
     return dp[m - 1][n - 1];
