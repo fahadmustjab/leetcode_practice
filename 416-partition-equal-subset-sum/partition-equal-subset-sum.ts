0fb3baa3e56c033b3dc9f40ff737bd861aa693e8
SillyCoder <beingfahad786@gmail.com>
2024-10-19 12:09:51 +0500

    Time: 200 ms (48.21%) | Memory: 83.6 MB (23.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.ts
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.ts
@@ -1,20 +1,21 @@
 function canPartition(nums: number[]): boolean {
     const n: number = nums.length;
-    const target = nums.reduce((acc, num) => acc + num, 0);
-    if (target % 2 != 0) return false;
-    const dp: number[][] = Array.from({ length: n }, () => Array((target / 2) + 1).fill(-1));
-
-    function backtrack(index: number, target: number) {
-        if (target === 0) {
-            return true;
-        }
-        if (index >= n || target < 0) return false;
-        if (dp[index][target] != -1) return dp[index][target] === 1;
-        const take = backtrack(index + 1, target - nums[index]);
-        const notTake = backtrack(index + 1, target);
-        dp[index][target] = (take || notTake) ? 1 : 0
-        return dp[index][target] === 1;
+    const totalSum = nums.reduce((acc, num) => acc + num, 0);
+    if (totalSum % 2 != 0) return false;
+    const partition = totalSum/2;
+    const dp: boolean[][] = Array.from({ length: n }, () => Array(partition + 1).fill(false));
+    for(let i =0; i < n;i++){
+        dp[i][0] = true;
     }
-    return backtrack(0, target / 2);
-
+     if (nums[0] <= partition) {
+        dp[0][nums[0]] = true;
+    }
+    for(let i =1;i <n;i++){
+        for(let target=0;target<=partition;target++){
+            const take = (target >= nums[i]) ? dp[i-1][target-nums[i]]: false;
+            const notTake = dp[i-1][target];
+            dp[i][target] = take || notTake;
+        }
+    }
+    return dp[n-1][partition];
 };
