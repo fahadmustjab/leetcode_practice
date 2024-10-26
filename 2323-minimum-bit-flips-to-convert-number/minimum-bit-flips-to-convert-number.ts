8bb9e4089b116c8fc4c3258bdee4368ef9970900
SillyCoder <beingfahad786@gmail.com>
2024-10-26 23:37:04 +0500

    Time: 0 ms (100.00%) | Memory: 51.8 MB (14.51%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.ts
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.ts
@@ -2,8 +2,8 @@ function minBitFlips(start: number, goal: number): number {
     let result = start ^ goal;
     let count = 0;
     while(result != 0){
-        result = result & (result-1);
-        count++;
+       count += result &1;
+       result = result >> 1;
     }
     return count;
 };
