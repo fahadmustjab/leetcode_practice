cc14c075482c52d21e5d6113b78972100cf2473e
SillyCoder <beingfahad786@gmail.com>
2024-11-03 20:58:49 +0500

    Time: 34 ms (38.10%) | Memory: 63 MB (48.39%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.ts
@@ -0,0 +1,17 @@
+function totalFruit(fruits: number[]): number {
+    const myMap: Map<number, number> = new Map(), n = fruits.length, k = 2;
+    let r = 0, max = 0, l = 0;
+    while (r < n) {
+        myMap.set(fruits[r],(myMap.get(fruits[r]) ?? 0)+1);
+        while (myMap.size > k) {
+            myMap.set(fruits[l], myMap.get(fruits[l]) - 1);
+            if (myMap.get(fruits[l]) == 0) {
+                myMap.delete(fruits[l]);
+            }
+            l++;
+        }
+        max = Math.max(max, r - l + 1);
+        r++;
+    }
+    return max;
+};
