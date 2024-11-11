c6a25c5135f134c6013ca68757679f22fc7545b8
SillyCoder <beingfahad786@gmail.com>
2024-11-12 00:20:25 +0500

    Time: 0 ms (100.00%) | Memory: 52.1 MB (38.69%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function zigzagLevelOrder(root: TreeNode | null): number[][] {
+    const result: number[][] = [];
+    if (root === null) return result;
+
+    const queue = new Queue();
+    queue.push(root);
+    let flipBit = false;
+    while (!queue.isEmpty()) {
+        const size = queue.size();
+        const array = [];
+        for (let i = 0; i < size; i++) {
+            const node: TreeNode | null = queue.front()!;
+            queue.dequeue();
+            if (node === null) continue;
+
+            if (flipBit) {
+                array.unshift(node.val);
+            } else {
+                array.push(node.val);
+            }
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        flipBit = !flipBit;
+        result.push([...array]);
+    }
+    return result;
+};
