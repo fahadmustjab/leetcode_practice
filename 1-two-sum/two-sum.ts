a97581d75ba27dddc2ea4fd045c6928762abfdc5
SillyCoder <beingfahad786@gmail.com>
2024-11-25 23:40:23 +0500

    Time: 9 ms (44.17%) | Memory: 52.7 MB (40.80%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,11 +1,16 @@
 function twoSum(nums: number[], target: number): number[] {
-    const hashMap = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const second = target - nums[i];
-        if (hashMap.get(second) >= 0 && hashMap.get(second) != i) {
-            return [i, hashMap.get(second)];
+    const pairs: [number, number][] = nums.map((num, index) => [num, index]);
+    pairs.sort((a, b) => a[0] - b[0]);
+    let l = 0, r = pairs.length - 1;
+    while (l < r) {
+        const sum = pairs[l][0] + pairs[r][0];
+        if (sum === target) {
+            return [pairs[l][1], pairs[r][1]];
+        } else if (sum > target) {
+            r--;
+        } else {
+            l++;
         }
-        hashMap.set(nums[i], i);
     }
-    return [-1, -1];
+    return [];
 };
