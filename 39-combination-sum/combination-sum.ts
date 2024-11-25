57ed5243d376cd494376929d07ae3d1a7a301249
SillyCoder <beingfahad786@gmail.com>
2024-11-26 00:53:36 +0500

    Time: 6 ms (42.83%) | Memory: 55.9 MB (40.98%) - LeetSync

--- 39-combination-sum/combination-sum.ts
+++ 39-combination-sum/combination-sum.ts
@@ -1,21 +1,19 @@
 function combinationSum(candidates: number[], target: number): number[][] {
-    candidates.sort((a, b) => a - b);
-    const ans: number[][] = [];
-    const t: number[] = [];
-    const dfs = (i: number, s: number) => {
-        if (s === 0) {
-            ans.push(t.slice());
+    const result: number[][] = [];
+    function helper(index: number, perms: number[], sum: number) {
+        if (sum > target) {
             return;
         }
-        if (s < candidates[i]) {
+        if (sum === target) {
+            result.push([...perms]);
             return;
         }
-        for (let j = i; j < candidates.length; ++j) {
-            t.push(candidates[j]);
-            dfs(j, s - candidates[j]);
-            t.pop();
+        for (let i = index; i < candidates.length; i++) {
+            perms.push(candidates[i]);
+            helper(i, perms, sum + candidates[i]);
+            perms.pop();
         }
-    };
-    dfs(0, target);
-    return ans;
-}
+    }
+    helper(0, [], 0);
+    return result;
+};
