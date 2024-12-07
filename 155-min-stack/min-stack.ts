7706c7ddab909d9a515e63748d992eb187f5fa87
SillyCoder <beingfahad786@gmail.com>
2024-12-07 13:44:13 +0500

    Time: 13 ms (46.68%) | Memory: 61.3 MB (17.70%) - LeetSync

---
+++ 155-min-stack/min-stack.ts
@@ -0,0 +1,37 @@
+class MinStack {
+    private min: number = Number.MAX_SAFE_INTEGER;
+    private stack;
+    constructor() {
+        this.stack = [];
+    }
+
+    push(val: number): void {
+        if (this.stack.length === 0) {
+            this.stack.push([val, val])
+        } else {
+            this.stack.push([val, Math.min(val, this.stack[this.stack.length - 1][1])]);
+
+        }
+    }
+
+    pop(): void {
+        this.stack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1][0];
+    }
+
+    getMin(): number {
+        return this.stack[this.stack.length - 1][1]
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
