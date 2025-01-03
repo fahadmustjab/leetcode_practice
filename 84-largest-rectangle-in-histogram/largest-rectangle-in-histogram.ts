9e0ad3b0996c0d5c4ba42f0cbc0c7771fcf67e2e
SillyCoder <beingfahad786@gmail.com>
2025-01-03 16:50:28 +0500

    Time: 19 ms (67.76%) | Memory: 62.3 MB (79.78%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.ts
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.ts
@@ -1,45 +1,24 @@
 function largestRectangleArea(heights: number[]): number {
     const n = heights.length;
-    const pse = previousSmallerElement(heights, n);
-    const nse = nextSmallerElement(heights, n);
-    let maxArea = 0;
-
+    let stack = [], maxArea = 0;
     for (let i = 0; i < n; i++) {
-        let area = heights[i] * (nse[i] - pse[i] - 1)
-        maxArea = Math.max(maxArea, area);
+        while (stack.length > 0 && heights[stack[stack.length - 1]] > heights[i]) {
+            let element = stack[stack.length - 1];
+            stack.pop();
+            let nse = i;
+            let pse = stack.length > 0 ? stack[stack.length - 1] : -1;
+            maxArea = Math.max(maxArea, heights[element] * (nse - pse - 1));
+        }
+        stack.push(i);
+    }
+
+    while (stack.length > 0) {
+        let nse = n;
+        let element = stack[stack.length - 1];
+        stack.pop();
+        let pse = stack.length > 0 ? stack[stack.length-1] : -1;
+        maxArea = Math.max(maxArea,heights[element]* (nse - pse - 1));
     }
     return maxArea
 };
 
-function previousSmallerElement(heights: number[], n: number) {
-    let stack = [], pse = Array(n).fill(-1);
-    for (let i = 0; i < n; i++) {
-        while (stack.length > 0 && heights[stack[stack.length - 1]] >= heights[i]) {
-            stack.pop();
-        }
-        if (stack.length) {
-            pse[i] = stack[stack.length - 1];
-        }
-        stack.push(i);
-    }
-    return pse;
-}
-
-
-
-function nextSmallerElement(heights: number[], n: number) {
-    let stack = [], nse = Array(n).fill(n);
-    for (let i = n - 1; i >= 0; i--) {
-        while (stack.length > 0 && heights[stack[stack.length - 1]] >= heights[i]) {
-            stack.pop();
-        }
-        if (stack.length) {
-            nse[i] = stack[stack.length - 1];
-        }
-        stack.push(i);
-    }
-    return nse;
-}
-
-// [-1,-1,1,5,1,2] pse
-// [1,6,2,2,6,6] nse
