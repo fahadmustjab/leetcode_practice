41f9556524a3816df5123baf4dc1715d69909f21
SillyCoder <beingfahad786@gmail.com>
2025-10-28 01:28:33 +0500

    Time: 1 ms (3.31%) | Memory: 55.2 MB (53.72%) - LeetSync

---
+++ Dynmaic-Programming/1236-n-th-tribonacci-number/n-th-tribonacci-number.ts
@@ -0,0 +1,11 @@
+function tribonacci(n: number): number {
+    if(n === 0)return 0;
+    if(n <= 2)return 1;
+    const dp = Array(n).fill(1);
+    dp[0] = 0;
+    for(let i = 3; i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+    console.log('here');
+    return dp[n];
+};
